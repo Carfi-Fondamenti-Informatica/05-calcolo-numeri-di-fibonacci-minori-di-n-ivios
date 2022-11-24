8f615f1e13df7ee880f60ada17b6d4533d9bfd0e
Ivio Feng <115487784+ivios@users.noreply.github.com>
2022-11-24 23:41:55 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n=0;
+    cin >> n;
+    int x=1, y=0, z=0;
+        if (n!=0) {
+            cout << x << endl;
+            for (int i = 1; i <= n; i++) {
+                z = x;
+                x = x + y;
+                y = z;
+                if (x > n) {
+                    break;
+                }
+                cout << x << endl;
+            }
+        }
+    return 0;
 }
